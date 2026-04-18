40fde4a04a4411b5a1f40cde01f43c9e5502820e
th845120 <th845120@gmail.com>
2026-04-18 09:18:20 +0000

    debug voice full

--- api/debug.js
+++ api/debug.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
   res.json({
-    voice: process.env.MINIMAX_VOICE_ID?.slice(0, 20) + "...",
+    voice_full: process.env.MINIMAX_VOICE_ID,
     group: process.env.MINIMAX_GROUP_ID,
     hasKey: !!process.env.MINIMAX_API_KEY,
   });
