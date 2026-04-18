d8d8429b95fd7e3d71cbac46f39ca06431cb003d
th845120 <th845120@gmail.com>
2026-04-18 09:19:06 +0000

    Remove debug endpoint

--- api/debug.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  res.json({
-    voice_full: process.env.MINIMAX_VOICE_ID,
-    group: process.env.MINIMAX_GROUP_ID,
-    hasKey: !!process.env.MINIMAX_API_KEY,
-  });
-}
